fb41dee2522ddc2a7c76f4f8c1594efedcd806ac
Luis <next313@hotmail.com>
2023-09-26 08:09:07 -0300

    Files created and modified

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+// Importe as dependências necessárias se houver alguma
+import React from 'react';
+
+// Defina o componente Home
+function Home() {
+    return <h1>My first page</h1>;
+}
+
+// Exporte o componente Home
+export default Home;
